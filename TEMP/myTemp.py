5d8cf380b38626ac87b1451072173d2806741ce9
joukeHijlkema <hylkema@free.fr>
2017-07-15 13:19:46 +0200

    bug fix in temp

--- TEMP/myTemp.py
+++ TEMP/myTemp.py
@@ -24,9 +24,9 @@ class myTemp(threading.Thread):
             os.system('modprobe w1-gpio')
             os.system('modprobe w1-therm')
         
-            self.base_dir      = '/sys/bus/w1/devices/'
-            self.device_folder = glob.glob(base_dir + '28*')[0]
-            self.device_file   = device_folder + '/w1_slave'
+            base_dir          = '/sys/bus/w1/devices/'
+            device_folder     = glob.glob(base_dir + '28*')[0]
+            self.device_file  = device_folder + '/w1_slave'
             
         self.newData  = signal("Temp")
         self.Doit     = True
